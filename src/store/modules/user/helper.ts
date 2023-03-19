62c813533b8ee6b9daf71a7b10701ec04f3efe48
ferch <fanchunhua116@gmail.com>
2023-03-20 00:14:03 +0800

    change avatar

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      avatar: 'http://lacrimosa.cn/static/oss/avatar.jpg',
+      name: 'ferch',
+      description: 'Star on <a href="http://blog.larcimosa.cn" class="text-blue-500" target="_blank" >blog</a>',
     },
   }
 }
