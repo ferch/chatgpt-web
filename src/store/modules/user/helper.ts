fd5cbf61a4a20dfd9f82fd9ce68869237286b706
ferch <fanchunhua116@gmail.com>
2023-03-20 00:05:38 +0800

    Revert "change avatar"
    This reverts commit d06d9fd8b2db15076d771f966edd18c9df793580.

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'http://lacrimosa.cn/static/oss/avatar.jpg',
-      name: 'ferch',
-      description: 'Star on <a href="http://blog.lacrimosa.cn/" class="text-blue-500" target="_blank" >blog</a>',
+      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
+      name: 'ChenZhaoYu',
+      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
     },
   }
 }
